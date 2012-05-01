47fd83a2c535af74987375f0705a6371312c48e0
Tama Eguchi <tama.eguchi@gmail.com>
2012-05-02 05:27:06 +0900

    Google Code Jam 2012 Round 1A Problem A. ソース整理&高速化

--- CodeJam2012/Round-1A/A/solve.py
+++ CodeJam2012/Round-1A/A/solve.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python
-# -*- coding:utf-8 -*-
+# -*- mode:python; coding:utf-8; indent-tabs-mode:nil -*-
 #
 # Problem A. Password Problem
 # http://code.google.com/codejam/contest/1645485/dashboard#s=p0
@@ -9,63 +9,65 @@ import sys
 
 
 def solve(length, correctly):
-	inputed = len(correctly)
+    inputed = len(correctly)
 
-	# N文字目で間違ってる可能性
-	wrong = []
-	left = 1
-	for rate in correctly:
-		wrong.append(left - left * rate)
-		left *= rate
+    # N文字目でミスタイプする確率
+    wrong = []
+    left = 1    # 残り
+    for rate in correctly:
+        # ミスタイプする確率
+        wrong.append(left * (1 - rate))
+        # 正しく入力して次の文字へ進む確率
+        left *= rate
 
-	def typecount(bs):
-		# backspaceをbs回タイプ + bs文字入力し直し + 残り文字 + [Enter]
-		return bs + bs + (length - inputed) + 1
+    # backspace入力回数別期待値
+    expected = []
+    for bs in range(inputed + 1):
+        # backspaceをbs回タイプ + bs文字入力し直し + 残り文字 + [enter]
+        count = bs + bs + (length - inputed) + 1
+        # backspaceで消せなかったミスタイプがある確率
+        misstype = sum(wrong[:inputed - bs])
+        expected.append(
+            # backspace で戻ってもミスが残ったままでもう一度入力し直す場合
+            (count + length + 1) * misstype
+            # backspace で戻って入力し直すと正解する場合
+            + count * (1 - misstype))
 
-	expected = []
-	# backspace入力回数
-	for backspace in range(inputed + 1):
-		# 全て正しく入力していた場合
-		count = [(typecount(backspace), left)]
-		#print count[-1]
-		# pos文字目で間違っている場合
-		for pos, rate in enumerate(wrong):
-			if pos < (inputed - backspace):
-				# 間違いを訂正できない
-				count.append((typecount(backspace) + length + 1, rate))
-			else:
-				count.append((typecount(backspace), rate))
-			#print pos, count[-1]
-		# 平均
-		expected.append(sum(cnt * rate for cnt, rate in count))
-		#print '#' * 5, backspace, expected[-1]
-	# このまま [Enter] して入力し直す
-	expected.append(1 + length + 1)
+    # このまま [enter] して入力し直す
+    expected.append(1 + length + 1)
 
-	return min(expected)
+    # 最小の期待値を回答
+    return min(expected)
 
 
 def main(INPUT, OUTPUT):
-	T = int(INPUT.readline())
-	for index in range(T):
-		A, B = map(int, INPUT.readline().strip().split())
-		correctly = map(float, INPUT.readline().strip().split())
-		OUTPUT.write('Case #%d: %f\n' % (index + 1, solve(B, correctly)))
+    T = int(INPUT.readline())
+    for index in range(T):
+        # 進捗表示
+        sys.stderr.write('#%d\r' % (index + 1))
+        A, B = map(int, INPUT.readline().strip().split())
+        correctly = map(float, INPUT.readline().strip().split())
+        OUTPUT.write('Case #%d: %f\n' % (index + 1, solve(B, correctly)))
 
 
-def makesample(Amax=3, Bmax=100, T=20):
-	import random
-	print T
-	for index in range(T):
-		A = random.randint(1, Amax)
-		B = random.randint(A, Bmax)
-		print A, B
-		print ' '.join('%f' % (random.randint(0, 100) / 100.0) for n in range(A))
+def makesample(Amax, Bmax, T=20):
+    import random
+    print T
+    for index in range(T):
+        A = random.randint(1, Amax)
+        B = random.randint(A, Bmax)
+        print A, B
+        print ' '.join('%f' % (random.randint(0, 100) / 100.0) for n in range(A))
 
 
 if __name__ == '__main__':
-	if '-makesample' in sys.argv[1:]:
-		makesample()
-	else:
-		main(sys.stdin, sys.stdout)
+    if len(sys.argv) > 1:
+        if ('-makesample', 'small') == tuple(sys.argv[1:]):
+            makesample(3, 100)
+        elif ('-makesample', 'large') == tuple(sys.argv[1:]):
+            makesample(99999, 100000)
+        else:
+            print 'usage: %s [-makesample <small|large>]' % sys.argv[0]
+    else:
+        main(sys.stdin, sys.stdout)
 
