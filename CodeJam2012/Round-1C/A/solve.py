dab410c3c76cf9bf35d8bdff51602a5d9ec6b940
Tama Eguchi <tama.eguchi@gmail.com>
2012-05-17 00:14:09 +0900

    Google Code Jam 2012 Round 1C Problem A. 高速化した別解

--- CodeJam2012/Round-1C/A/solve.py
+++ CodeJam2012/Round-1C/A/solve.py
@@ -15,43 +15,30 @@ class Class:
 
 
 def solve(classes):
-    found = set()
+    # クラス→スーパークラス のリストから、クラスID→サブクラスIDのリスト を構築する
+    subclassmap = dict((cls.no, set()) for cls in classes)
+    for cls in classes:
+        for clsid in cls.inherits:
+            # スーパークラスのサブクラスIDリストに自身のクラスIDを加える
+            subclassmap[clsid].add(cls.no)
 
-    for index, cls in enumerate(classes):
-        # index → no
-        no = index + 1
-        if not filter(lambda c: no in c.inherits, classes):
-            # このクラスを継承しているクラスがない(サブクラスの終端)
+    # ルートクラスごとに子孫を調べる
+    for root in [cls.no for cls in classes if not cls.inherits]:
+        descendant = list(subclassmap[root])
+        index = 0
+        while index < len(descendant):
+            # 次に調べるクラスのID
+            clsid = descendant[index]
+            # サブクラスを列挙
+            for subclass in subclassmap[clsid]:
+                if subclass in descendant:
+                    # 既に同じサブクラスに辿り着いている
+                    return "Yes"
+                # サブクラスリストに追加
+                descendant.append(subclass)
+            index += 1
 
-            # 再帰すると Large で死ぬのでループで回す
-            # (クラス, 親クラス走査イテレータ) のペアをリストする
-            route = [(cls, iter(cls.inherits))]
-            while route:
-                cls, it = route[-1]
-
-                if not cls.inherits:
-                    # ルートクラス
-                    key = (route[0][0].no, route[-1][0].no)
-                    if key in found:
-                        # サブクラス&ルートクラスペアが既にある = 2つの経路が見つかった
-                        return "Yes"
-                    # サブクラス&ルートクラスペアを保持
-                    found.add(key)
-                    # このルートクラスを取り除いてひとつ上の経路へ
-                    route.pop()
-
-                else:
-                    try:
-                        # 次のクラス
-                        nextno = it.next()
-                        cls = classes[nextno-1]
-                        # 敬称ルートに積む
-                        route.append((cls, iter(cls.inherits)))
-                    except StopIteration:
-                        # このクラスの継承走査は終わった → ひとつ上の次へ
-                        route.pop()
-
-    # サブクラス&ルートクラスペアの重複がなかった
+    # 同じサブクラスに辿り着かなかった
     return "No"
 
 
